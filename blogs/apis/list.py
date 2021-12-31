96736353517b0a80a7574559e5938829e42ca0c4
limoiie <limo.iie4@gmail.com>
2021-12-31 10:44:46 +0800

    refactor: Provide url for counting blogs

--- blogs/apis/list.py
+++ blogs/apis/list.py
@@ -3,6 +3,12 @@ from django.http import JsonResponse
 from blogs.models import BlogWrapper, Blog
 
 
+def _response_count(count):
+    return JsonResponse(data={
+        'data': count
+    })
+
+
 def _response_list(state, msg, data):
     return JsonResponse(data={
         'state': state,
@@ -11,10 +17,17 @@ def _response_list(state, msg, data):
     })
 
 
-def list_blogs(request, page_num, page_size):
-    offset = (page_num - 1) * page_size
+def count_blogs(_request):
+    return _response_count(Blog.objects.count())
+
+
+def list_blogs(_request, page_num, page_size):
+    offset = page_num * page_size
 
     blogs = Blog.objects.order_by('-create_time')
     blogs = blogs[offset:offset+page_size]
     data = [BlogWrapper.blog2json(blog) for blog in blogs]
-    return _response_list(True, 'successful', data)
+    return _response_list(True, 'successful', {
+        'page': data,
+        'count': Blog.objects.count()
+    })
--- blogs/urls.py
+++ blogs/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('auth/', login.login, name='login'),
     path('publish/', publish.publish_blog, name='publish'),
     path('list/<int:page_num>/<int:page_size>', list.list_blogs, name='list'),
+    path('count/', list.count_blogs, name='count'),
     path('<int:blog_id>/', detail.detail_blog, name='details'),
     path('csrftoken/', csrftoken.csrf_token, name='csrf')
 ]
