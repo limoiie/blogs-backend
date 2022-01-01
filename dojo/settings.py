580c2dbb48f2fc1dd9a3d8df0b8dfa1b55e0edb0
limoiie <limo.iie4@gmail.com>
2022-01-01 18:30:04 +0800

    chore: Change log level to debug

--- dojo/settings.py
+++ dojo/settings.py
@@ -31,6 +31,20 @@ ALLOWED_HOSTS = [
     '127.0.0.1', 'localhost'
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
 
 # Application definition
 
